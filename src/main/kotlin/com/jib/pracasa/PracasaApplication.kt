de88ca5371a586c4baadfca741ea19078ebc518e
joelma <joelmadev@gmail.com>
2022-05-19 11:16:42 -0300

    Jo-1 projeto inicial

---
+++ src/main/kotlin/com/jib/pracasa/PracasaApplication.kt
@@ -0,0 +1,11 @@
+package com.jib.pracasa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracasaApplication
+
+fun main(args: Array<String>) {
+	runApplication<PracasaApplication>(*args)
+}
---
+++ src/test/kotlin/com/jib/pracasa/PracasaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jib.pracasa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracasaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
